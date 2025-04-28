e895e14aaf6f542a83bc28a8518b87d02e92616f
Aoang <aoang@x2oe.com>
2025-04-28 09:43:59 +0800

    chore: format

--- main.ts
+++ main.ts
@@ -2,7 +2,10 @@ import * as http from "@std/http";
 
 Deno.serve((req) => {
   const pathname = new URL(req.url).pathname;
-  if (pathname.startsWith("/Modules") || pathname.startsWith("/Ruleset") || pathname.startsWith("/Script")) {
+  if (
+    pathname.startsWith("/Modules") || pathname.startsWith("/Ruleset") ||
+    pathname.startsWith("/Script")
+  ) {
     return http.serveDir(req);
   }
 
